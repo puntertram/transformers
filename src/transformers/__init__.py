2ac7c6231888351283a85c33b49d1410a8b71d37
Puneeth N Naik <puneethnaik60@gmail.com>
2023-05-31 16:18:21 +0530

    watermark to indicate our changes

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -45,7 +45,7 @@ from .utils import (
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
 
-print("[transformers] This is the custom changes from branch minimum_profiler_overhead_opt4...")
+print("[transformers] This is the custom changes from branch minimum_profiler_overhead_opt7...")
 
 # Base objects, independent of any specific backend
 _import_structure = {
